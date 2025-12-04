22ca27270e46ccd5df4863c0ecd0bc3a8d7f4a9d
Dev2Mist <benjaminborges.dev@gmail.com>
2025-12-04 10:40:31 -0300

    :sparkles:Añado configuraciones de ui5

--- webapp/Component.js
+++ webapp/Component.js
@@ -8,6 +8,9 @@ sap.ui.define(["sap/ui/core/UIComponent"], function (UIComponent) {
 
     init: function () {
       // Llama a la función init del padre
+      this.ownerComponent.setModel(new ResourceModel({
+          bundleName: "artech.capacitacion.i18n.i18n" // <--- CAMBIA ESTO por tu ruta real
+      }), "i18n");
       UIComponent.prototype.init.apply(this, arguments);
     },
   });
